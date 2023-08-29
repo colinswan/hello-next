8f5c498375ddb5e5a26748acd884e68c8f7abc81
Colin Swan <colinswan.cs@gmail.com>
2023-08-29 19:53:32 +0100

    completed exercise code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          <p> Copyright ©️ {new Date().getFullYear()} </p>
+        </footer>
       </body>
     </html>
   );
